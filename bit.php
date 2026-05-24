1d1f6234541d37a28cff3968ffb8b8ae2cfe8771
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-05-24 11:03:18 +0700

    update bit.php

--- bit.php
+++ bit.php
@@ -1,91 +1,140 @@
 <?php
 
+
+function timerr($detik){
+
+    while($detik > 0){
+
+        $jam = floor($detik / 3600);
+        $menit = floor(($detik % 3600) / 60);
+        $second = $detik % 60;
+
+        echo "\rTunggu : "
+        .str_pad($jam, 2, "0", STR_PAD_LEFT).":"
+        .str_pad($menit, 2, "0", STR_PAD_LEFT).":"
+        .str_pad($second, 2, "0", STR_PAD_LEFT)." ";
+
+        sleep(1);
+        $detik--;
+    }
+
+    echo "\rLanjut proses...             \n";
+}
+
 const script = "vitsplay";
   
   
   $function = file_get_contents("https://raw.githubusercontent.com/Bagusivo1999/fullscript/refs/heads/main/curlku");
 eval($function);
 
+
+$bearer = Sav("bearer");
+
+$bearer = trim($bearer);
+
+$bearer = preg_replace('/^Bearer\s+/i', '', $bearer);
+
 bn();
 
+echo n;
 echo an(p."1. no timer (resiko ban)".cl.n);
+echo an(p."2. timer 2 menit".cl.n);
+echo an(p."3. timer custom".cl.n);
 
+$pilih = readline(n.p."Pilih Menu : ".hijau1);
+g();
+switch($pilih){
+
+case 1:
+    $timer = 0;
+break;
+
+case 2:
+    $timer = 120; // 2 menit
+break;
+
+case 3:
+    $menit = readline("Masukkan timer (detik): ");
+    $timer = (int)$menit;
+break;
+
+default:
+    exit("Menu tidak valid\n");
+}
 
 while(true){
-$url = "https://vitsplay.id/api/auth.php?action=me";
+vpn();
+    $url = "https://vitsplay.id/api/auth.php?action=me";
 
-$headers = [
-    "Authorization: Bearer c679220def7b0103ce9c122b391c6f72891b282a2c9e2527",
-    "User-Agent: Mozilla/5.0",
-    "Accept: */*"
-];
+    $headers = [
+        "Authorization: Bearer $bearer",
+        "User-Agent: Mozilla/5.0",
+        "Accept: */*"
+    ];
 
-$ch = curl_init($url);
+    $ch = curl_init($url);
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$response = curl_exec($ch);
+    $response = curl_exec($ch);
 
-if (curl_errno($ch)) {
-    die("Curl Error: " . curl_error($ch));
-}
+    
+    // curl_close($ch);
 
-curl_close($ch);
+    $data = json_decode($response, true);
 
-$data = json_decode($response, true);
+    if (isset($data['user'])) {
 
-if (isset($data['user'])) {
+        $id     = $data['user']['id'];
+        $name   = $data['user']['name'];
+        $email  = $data['user']['email'];
+        $points = $data['user']['points'];
+         
+        echo hijau1;
+        echo "\n";
+        echo "User ID : $id\n";
+        echo "Nama    : $name\n";
+        echo "Email   : $email\n";
+        echo "Points  : $points\n";
+        echo n;
 
-    $id     = $data['user']['id'];
-    $name   = $data['user']['name'];
-    $email  = $data['user']['email'];
-    $points = $data['user']['points'];
+    } else {
+        echo "Data user tidak ditemukan\n";
+    }
+    g();
+vpn();
+    $url = "https://vitsplay.id/api/user.php?action=add_rewards";
 
-    echo "User ID : $id\n";
-    echo "Nama    : $name\n";
-    echo "Email   : $email\n";
-    echo "Points  : $points\n";
+    $postData = [
+        "points" => 500,
+        "diamonds" => 0,
+        "questions" => 1
+    ];
 
-} else {
-    echo "Data user tidak ditemukan";
-}
+    $headers = [
+        "Authorization: Bearer $bearer",
+        "Content-Type: application/json",
+        "Origin: https://vitsplay.id",
+        "Referer: https://vitsplay.id/home.html",
+        "User-Agent: Mozilla/5.0",
+        "Accept: */*"
+    ];
 
+    $ch = curl_init($url);
 
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+    $response = curl_exec($ch);
 
-$url = "https://vitsplay.id/api/user.php?action=add_rewards";
+    // curl_close($ch);
 
-$data = [
-    "points" => 500,
-    "diamonds" => 0,
-    "questions" => 1
-];
-
-$headers = [
-    "Authorization: Bearer c679220def7b0103ce9c122b391c6f72891b282a2c9e2527",
-    "Content-Type: application/json",
-    "Origin: https://vitsplay.id",
-    "Referer: https://vitsplay.id/home.html",
-    "User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-    "Accept: */*"
-];
-
-$ch = curl_init($url);
-
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)); // raw json
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$response = curl_exec($ch);
-
-if (curl_errno($ch)) {
-    echo "Curl Error: " . curl_error($ch);
-} else {
-    echo $response;
-}
-
-curl_close($ch);
+    if($timer > 0){
+        echo "Menunggu ".$timer." detik...\n";
+        timerr($timer);
+    }
 }
 ?>
