f136403d69a232bb2eac5bac69d6f4fb628f5045
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-06-19 19:51:48 +0700

    update bot1.php

--- bot1.php
+++ bot1.php
@@ -130,8 +130,6 @@ function menu() {
                 sleep($delay);
             }
         } elseif ($pilih == '4') {
-           system("xdg-open https://t.me/modegratis19");
-        } elseif ($pilih == '5') {
         bn();
             exit("Keluar\n");
         } else {
