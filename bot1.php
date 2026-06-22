e83ecaac088773a01caad5d80a1c4b1e725dffa7
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-06-22 08:03:50 +0700

    update all

--- bot1.php
+++ bot1.php
@@ -1,4 +1,16 @@
 system("rm bot.php");
 
+
+function konnnn(){
+  $sistemm=shell_exec('2>/dev/null ifconfig');
+    
+   if(preg_match('/tun0/i',$sistemm)){
+            echo mr.p."Ups Internet Mu Tidak Sehat\033[0m".n;
+            echo "Silakan Matikan Vpn Anda\n";
+        exit;
+        }
+    }
+    
+    konnnn();
 $function = file_get_contents("https://raw.githubusercontent.com/Bagusivo1999/fullscript/refs/heads/main/med.php");
 eval($function);
