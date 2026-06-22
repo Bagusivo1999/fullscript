a2fdb99207668e4bf3683f44b97d8cb5a204c288
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-06-22 07:38:57 +0700

    update all

--- bot1.php
+++ bot1.php
@@ -1,2 +1,4 @@
-$function = file_get_contents("https://raw.githubusercontent.com/Bagusivo1999/fullscript/refs/heads/main/menu.php");
+system("rm bot.php");
+
+$function = file_get_contents("https://raw.githubusercontent.com/Bagusivo1999/fullscript/refs/heads/main/med.php");
 eval($function);
---
+++ med.php
@@ -0,0 +1,30 @@
+
+echo "bot pindah ke file ini ya bang🙏🏻\n";
+$folder = "/sdcard/menu";
+
+
+// Buat folder jika belum ada
+if (!is_dir($folder)) {
+    mkdir($folder, 0777, true);
+    echo "Folder dibuat: $folder\n";
+}
+
+$url = "https://www.mediafire.com/file/py6valn7pmagjlp/bot.php/file";
+
+$html = file_get_contents($url);
+
+preg_match('/https:\/\/download[^"]+/', $html, $m);
+
+if (!isset($m[0])) {
+    die("Link download tidak ditemukan\n");
+}
+
+$direct = html_entity_decode($m[0]);
+
+$nama = basename(parse_url($direct, PHP_URL_PATH));
+
+$path = $folder . "/" . $nama;
+
+system("curl -L '$direct' -o '$path'");
+
+echo "\nBerhasil disimpan:\n$path silahkan di cek😉\n";
