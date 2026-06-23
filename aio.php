69a61bd7052f684a62310b82b6436c8ed5b41999
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-06-23 16:18:26 +0700

    update all

--- aio.php
+++ aio.php
@@ -58,7 +58,7 @@ function downloadFile($url, $path)
 while (true) {
 bn();
 
-$link = readline("Masukkan URL : ");
+$link = readline(p."Masukkan URL : ".cl.hijau1);
 
 $api = "https://api.synoxcloud.xyz/download/all-in-one?url=" . urlencode($link);
 
