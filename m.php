d48372b46af7f3a9d402cb377606ecb4faf7e19b
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-06-28 19:58:33 +0700

    update all

--- m.php
+++ m.php
@@ -45,7 +45,7 @@ $menu = [
         "Cashclip" => "cash.php",
         "Cashclip 2" => "cash1.php",
         "Earntycoon" => "ty.php",
-        "Moneyrain (Tes di hp kalian)" => "moneyrain.php",
+        "Moneyrain" => "moneyrain.php",
         // "Earn Ltc Bot (comingsoon)" => ""
     ],
     "=== TOOLS SELAIN FAUCET ===" => [
