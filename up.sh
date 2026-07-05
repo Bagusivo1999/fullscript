3a3f290c8ba2650a1266caae7bc41fedad0d7bfb
Bagusivo1999 <bagusfildhonfatoni8@gmail.com>
2026-07-05 11:24:42 +0700

    update all

--- up.sh
+++ up.sh
@@ -1,12 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Set identity (cukup sekali, tapi biar aman)
-git config user.name "Bagusivo1999"
-git config user.email "bagusfildhonfatoni8@gmail.com"
-
-# Add, commit, push (langsung dari folder repo)
 git add .
-git commit -m "update all $(date '+%Y-%m-%d %H:%M')"
-git push -f origin main
 
-echo "✅ Selesai!"
+git commit -m "update all"
+
+git push origin main
